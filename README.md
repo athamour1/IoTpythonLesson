337cbcea7122be19f94d210d3abd675b306cbd98
athamour1 <thanosmour.tk@gmail.com>
2022-03-19 14:01:43 +0200

    implement: ptoing uppon new message from brocker

---
+++ README.md
@@ -0,0 +1,4 @@
+# this is a tile
+
+## Info list
+- [https://rat.socialenergy-project.eu/consumers/7?duration=604800&start_date=&end_date=&type=Real-time&interval_id=2](https://rat.socialenergy-project.eu/consumers/7?duration=604800&start_date=&end_date=&type=Real-time&interval_id=2)
--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import paho.mqtt.client as mqtt
-# import matplotlib.pyplot as plt
-# from matplotlib.widgets import Button
-# from threading import Timer
-# from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+from threading import Timer
+from datetime import datetime
 import ssl
 import os
 import sys
 
+
 class IoTExample:
     def __init__(self):
+        self.ax = None
         self._establish_mqtt_connection()
+        self._prepare_graph_window()
 
-    # call this method to start the client loop
+   # call this method to start the client loop
     def start(self):
-        self.client.loop_forever()
+        if self.ax:
+            self.client.loop_start()
+            plt.show()
+        else:
+            self.client.loop_forever()
 
     # call this method to disconnect from the broker
     def disconnect(self, args=None):
+        self.finishing = True
         self.client.disconnect()
 
     # disconnect
@@ -31,15 +39,52 @@ class IoTExample:
         self.client.connect('phoenix.medialab.ntua.gr', 8883)
 
     def _on_connect(self, client, userdata, flags, rc):
-        client.subscribe('hscnl/hscnl02/state/ZWaveNode005_ElectricMeterWatts/state')
+        client.subscribe(
+            'hscnl/hscnl02/state/ZWaveNode005_ElectricMeterWatts/state')
         client.subscribe('hscnl/hscnl02/command/ZWaveNode005_Switch/command')
         client.subscribe('hscnl/hscnl02/state/ZWaveNode005_Switch/state')
 
     def _on_message(self, client, userdata, msg):
+        if msg.topic == 'hscnl/hscnl02/state/ZWaveNode005_ElectricMeterWatts/state':
+            self._add_value_to_plot(float(msg.payload))
         print(msg.topic+' '+str(msg.payload))
 
     def _on_log(self, client, userdata, level, buf):
         print('log: ', buf)
+
+    def _my_timer(self):
+        self._refresh_plot()
+        if not self.finishing:
+            Timer(1.0, self._my_timer).start()
+
+    def _prepare_graph_window(self):
+        plt.rcParams['toolbar'] = 'None'
+        self.ax = plt.subplot(111)
+        self.dataX = []
+        self.dataY = []
+        self.first_ts = datetime.now()
+        self.lineplot, = self.ax.plot(
+            self.dataX, self.dataY, linestyle='--', marker='o', color='b')
+        self.ax.figure.canvas.mpl_connect('close_event', self.disconnect)
+        self.finishing = False
+        self._my_timer()
+
+    def _refresh_plot(self):
+        if len(self.dataX) > 0:
+            self.ax.set_xlim(min(self.first_ts, min(self.dataX)),
+                             max(max(self.dataX), datetime.now()))
+            self.ax.set_ylim(min(self.dataY) * 0.8, max(self.dataY) * 1.2)
+        else:
+            self.ax.set_xlim(self.first_ts, datetime.now())
+        self.ax.relim()
+        plt.draw()
+
+    def _add_value_to_plot(self, value):
+        self.dataX.append(datetime.now())
+        self.dataY.append(value)
+        self.lineplot.set_data(self.dataX, self.dataY)
+        self._refresh_plot()
+
 
 try:
     iot_example = IoTExample()
